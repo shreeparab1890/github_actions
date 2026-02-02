5661d86cf56d00b39bb26e52c9d31f375d516c2a
Shrikrishna Parab <40030115+shreeparab1890@users.noreply.github.com>
2026-02-02 11:19:01 +0530

    Replace sum function with sub in tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -8,7 +8,7 @@ def test_add():
 
     
 def test_sub():
-    assert sum(1,-1) == 2
-    assert sum(6,2) == 4
-    assert sum(3,3) == 0
-    assert sum(3,4) == -1
+    assert sub(1,-1) == 2
+    assert sub(6,2) == 4
+    assert sub(3,3) == 0
+    assert sub(3,4) == -1
