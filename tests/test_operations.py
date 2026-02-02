b3f15935f01203e409f3b78bc55407a8b597184b
shreeparab1890 <shrikrishnaparab18@gmail.com>
2026-02-02 11:02:28 +0530

    operatons added, unit tests added

---
+++ tests/test_operations.py
@@ -0,0 +1,14 @@
+from src.operations import add, sub
+
+def test_add():
+    assert add(1,3) == 4
+    assert add(5,5) == 10
+    assert add(4,5) == 9
+    assert sum(1,-1) == 0
+
+    
+def test_sub():
+    assert sum(1,-1) == 2
+    assert sum(6,2) == 4
+    assert sum(3,3) == 0
+    assert sum(3,4) == -1
